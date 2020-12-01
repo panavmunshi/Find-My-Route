0143116a6792afba06e75b82e98d284a97a11570
Panav Munshi <pmunshi2@illinois.edu>
2020-11-30 21:29:09 -0800

    DirectedGraph: [WIP] Create skeleton for DirectedGraph adt
    -> Basic structure is still subject to change.
    -> Need to implement functions. Will use 225's Graph adt as reference.

---
+++ libraries/DirectedGraph.cpp
@@ -0,0 +1,27 @@
+#include "DirectedGraph.h"
+
+using namespace cust_lib;
+
+bool DirectedGraph::adjacent(Vertex source, Vertex dest) {
+
+}
+
+vector<Vertex> DirectedGraph::neighbours(Vertex source) {
+
+}
+
+void DirectedGraph::add_vertex(Vertex toAdd) {
+
+}
+
+void DirectedGraph::remove_vertex(Vertex toRemove) {
+
+}
+
+void DirectedGraph::add_edge(Vertex source, Vertex dest, Edge* edge) {
+
+}
+
+vector<Edge*> get_edges(Vertex source, Vertex dest) {
+    
+}
