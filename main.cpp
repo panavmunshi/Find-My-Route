46e74662b4b0868110399f4649d8caf68091fef3
Panav Munshi <pmunshi2@illinois.edu>
2020-12-09 21:37:22 -0800

    base: Add traversal options to main.cpp

--- main.cpp
+++ main.cpp
@@ -12,13 +12,53 @@ int main() {
         cust_lib::Parser m_parser(file_loc);
 
         if((curr_graph = m_parser.read_file()) == NULL) {
-            cout << "INVALID FILE! Please input a valid string\n" << endl;
+            cout << "INVALID FILE LOCATION! Please input a valid string\n" << endl;
         } else {
             break;
         }
     }
 
+    cout << "Successfully read file! (Errors printed to console if any)" << endl;
+    cout << "\n/***********************************************/\n" << endl;
+
+    int opt_code = 0;
+    string args;
+    while (true) {
+        cout << "Please choose one of the following: " << endl;
+        cout << "Enter '1': Shortest Flight route from Point A to Pqoint B" << endl;
+        cout << "Enter '2': Shortest Flight route from Point A to Point B with constraints" << endl;
+        cout << "Enter '3': ~ To be filled for BFS ~" << endl;
+        
+        cin >> args;
+        try {
+            opt_code = std::stoi(args);
+            if (opt_code > 3 || opt_code < 1) {
+                cout << "Error: Invalid opt_code.  Please choose between option 1 to 3. \n" << endl;
+                continue;
+            } else {
+                break;
+            }
+        } catch(exception& e) {
+            cout << "Error: Invalid input. Please enter a number. \n" << endl;
+        }
+    }
+
+    switch (opt_code) {
+        case 1: {
+            break;
+        }
+
+        case 2: {
+            break;
+        }
+
+        case 3: {
+            break;
+        }
+    }
+
+    cout << "Successfully ending program" << endl;
+
     delete curr_graph;
-    cout << "Successfully read file!" << endl;
     return 0;
 }
