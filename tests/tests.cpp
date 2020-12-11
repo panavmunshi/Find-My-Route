1387601abbb7ae05122b1312827900454fb0fdef
Panav Munshi <pmunshi2@illinois.edu>
2020-12-11 05:40:22 -0800

    tests: Misc changes -> Add more test cases. -> Streamline testing

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,13 +1,7 @@
 #include "tests.h"
 using namespace cust_lib;
 
-void test_cases::test_graph() {
-    string loc = "dataset/1987.csv";
-
-    Parser curr_parser(loc);
-    Graph* test_obj = curr_parser.read_file();
-
-
+void test_cases::test_graph(Graph* test_obj) {
     /* Run test cases. */
     vector<Vertex> adjacent_nodes = test_obj->getAdjacent("SAV");
 
@@ -16,32 +10,22 @@ void test_cases::test_graph() {
     }
 }
 
-void test_cases::test_heuristic() {
-    string loc = "dataset/1987.csv";
-
-    Parser curr_parser(loc);
-    Graph* test_obj = curr_parser.read_file();
-
+void test_cases::test_heuristic(Graph* test_obj) {
     vector<Vertex> vertice_list = test_obj->getVertices();
 
     for (Vertex v : vertice_list) {
-        int return_val = test_obj->get_edge_count("SAV", v);
+        int return_val = test_obj->get_edge_count("SFO", v);
 
         if (return_val != INT_MAX) {
-            //cout << "From: " << v << " To: " << "SAV" << " Edges: " << return_val << endl;
-            cout << v << endl;
+            cout << "From: " << v << " To: " << "SFO" << " Edges: " << return_val << endl;
+            //cout << v << endl;
             
         }
     }
 
 }
 
-void test_cases::test_vertex() {
-    string loc = "dataset/1987.csv";
-
-    Parser curr_parser(loc);
-    Graph* test_obj = curr_parser.read_file();
-
+void test_cases::test_vertex(Graph* test_obj) {
     vector<Vertex> vertice_list = test_obj->getVertices();
 
     for (Vertex v : vertice_list) {
@@ -50,23 +34,41 @@ void test_cases::test_vertex() {
 
 }
 
-void test_cases::print_graph() {
-    string loc = "dataset/1987.csv";
-
-    Parser curr_parser(loc);
-    Graph* test_obj = curr_parser.read_file();
-
+void test_cases::print_graph(Graph* test_obj) {
     test_obj->print();
 }
 
-int main() {
-    printf("Testing Vertices. \n");
-    test_cases::test_vertex();
+void test_cases::test_a_star(Graph* test_obj) {
+    search_algos search_obj;
 
-    printf("Testing heuristics. \n");
-    test_cases::test_heuristic();
+    vector<Vertex> result = search_obj.search(test_obj, "PIH", "ATW", false);
+
+    for (Vertex v : result) {
+        cout << v << " -> ";
+    }
+    cout << endl;
+}
+
+int main() {
+    string loc = "dataset/1987.csv";
+
+    Parser curr_parser(loc);
+    Graph* search_obj = curr_parser.read_file();
+
+    /*printf("Testing Vertices. \n");
+    test_cases::test_vertex();*/
+
+    /*printf("Testing heuristics. \n");
+    test_cases::test_heuristic();*/
     //test_cases::test_graph();
     //test_cases::print_graph();
 
+    printf("Testing A star\n");
+    test_cases::test_a_star(search_obj);
+
+    /*printf("Testing heuristics. \n");
+    test_cases::test_heuristic(search_obj);*/
+
+    delete search_obj;
     return 0;
 }
--- tests/tests.h
+++ tests/tests.h
@@ -1,11 +1,13 @@
 #include "../libraries/parser.h"
+#include "../libraries/search_algos.h"
 
 namespace cust_lib {
     class test_cases {
         public:
-            static void test_graph();
-            static void test_heuristic();
-            static void test_vertex();
-            static void print_graph();
+            static void test_graph(Graph* obj);
+            static void test_heuristic(Graph* obj);
+            static void test_vertex(Graph* obj);
+            static void print_graph(Graph* obj);
+            static void test_a_star(Graph* obj);
     };
 }
