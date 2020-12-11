3c2887aa37b2b568f4b2f33402da6b25a33d17af
Panav Munshi <pmunshi2@illinois.edu>
2020-12-11 05:36:58 -0800

    search_algos: Implement Dijkstra and A* Search using priority queue. -> Adapted from https://www.redblobgames.com/pathfinding/a-star/implementation.html

--- libraries/search_algos.cpp
+++ libraries/search_algos.cpp
@@ -1,11 +1,54 @@
 #include "search_algos.h"
 
 using namespace cust_lib;
+using namespace std;
 
-vector<Edge> search_algos::a_star_search(Graph* search_graph, Vertex source, Vertex destination) {
-    return vector<Edge>();
+vector<Vertex> search_algos::search(Graph* search_graph, Vertex source, Vertex destination, bool isDijkstra) {
+    search_p_queue frontier;
+    frontier.put(source, 0);
+
+    std::unordered_map<Vertex, Vertex> parentToChildMap;
+    std::unordered_map<Vertex, int> vertexToCost;
+    std::vector<Vertex> toReturn;
+
+    parentToChildMap[source] = "NULL";
+    vertexToCost[source] = 0;
+
+    while (!frontier.empty()) {
+        Vertex curr = frontier.get();
+        if (curr == destination) {
+            break;
+        } else {
+            for (Vertex next : search_graph->getAdjacent(curr)) {
+                int new_cost = vertexToCost[curr] + search_graph->getEdgeWeight(curr, next);
+                if (vertexToCost.find(next) == vertexToCost.end() || new_cost < vertexToCost[next]) {
+                    vertexToCost[next] = new_cost;
+                    int priority = (isDijkstra) ? new_cost : new_cost + calc_heuristic(search_graph, source, destination);
+                    frontier.put(next, priority);
+                    parentToChildMap[next] = curr;
+                }
+            }
+        }
+    }
+
+    std::stack<Vertex> reversal_stack;
+    Vertex current = destination;
+
+    while (current != "NULL") {
+        reversal_stack.push(current);
+        if (parentToChildMap.find(current) == parentToChildMap.end()) {
+            return vector<Vertex>();
+        }
+        current = parentToChildMap[current];
+    }
+
+    while (!reversal_stack.empty()) {
+        toReturn.push_back(reversal_stack.top());
+        reversal_stack.pop();
+    }
+    return toReturn;
 }
 
-vector<Edge> search_algos::reconstruct_path(Vertex source, Vertex destination) {
-    return vector<Edge>();
+int search_algos::calc_heuristic(Graph* graph, Vertex A, Vertex B) {
+    return graph->get_edge_count(A, B);
 }
--- libraries/search_algos.h
+++ libraries/search_algos.h
@@ -1,12 +1,33 @@
 #pragma once
 #include "graph.h"
-#include "vector"
+#include <vector>
+#include <queue>
+#include <stack>
 
 namespace cust_lib {
     class search_algos {
         public:
-            static vector<Edge> a_star_search(Graph* search_graph, Vertex source, Vertex destination);
+            vector<Vertex> search(Graph* search_graph, Vertex source, Vertex destination, bool isDijkstra);
         private:
-            static vector<Edge> reconstruct_path(Vertex came_from, Vertex current);
+            int calc_heuristic(Graph* graph, Vertex A, Vertex B);
+            struct search_p_queue {
+                typedef std::pair<int, Vertex> node_elem;
+                
+                std::priority_queue<node_elem, std::vector<node_elem>, std::greater<node_elem>> node_queue;
+
+                inline bool empty() const {
+                    return node_queue.empty();
+                }
+
+                inline void put(Vertex item, int priority) {
+                    node_queue.emplace(priority, item);
+                }
+
+                Vertex get() {
+                    Vertex toReturn = node_queue.top().second;
+                    node_queue.pop();
+                    return toReturn;
+                }
+            };
     };
 }
