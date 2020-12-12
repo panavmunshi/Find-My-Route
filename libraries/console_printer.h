c2ea47532417d8046657e6c655cea0398595ab61
Panav Munshi <pmunshi2@illinois.edu>
2020-12-11 19:34:28 -0800

    console_printer: Implement library to print airport data to console.

---
+++ libraries/console_printer.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include "graph.h"
+
+using namespace std;
+
+void print_formatted(Graph* graph, vector<Vertex>& toPrint) {
+    size_t largest_len = 0;
+    vector<vector<string>> print_data;
+    print_data.push_back(vector<string>());
+
+    for (Vertex v : toPrint) {
+        print_data[0].push_back(v);
+    }
+
+    for (size_t idx = 1; idx < toPrint.size(); idx++) {
+        Edge flight_edge = graph->getEdge(toPrint[idx], toPrint[idx - 1]);
+        size_t num_flights = flight_edge.flight_details.size();
+        bool is_less = false;
+        if (print_data.size() - 1 < num_flights) {
+            int delta = num_flights - (print_data.size() - 1);
+            while (delta-- > 0) {
+                print_data.push_back(vector<string>());
+            }
+        } else if (print_data.size() - 1 > num_flights) {
+            is_less = true;
+        }
+
+        auto it = flight_edge.flight_details.begin();
+        for (size_t i = 0; i < num_flights; i++) {
+            stringstream dept, arr;
+            dept << setfill('0') << setw(4) << it->second.first;
+            arr << setfill('0') << setw(4) << it->second.second;
+
+            string flight_detail = it->first + " -> Dep: " + dept.str() + " -- Arr: " + arr.str();
+            size_t flight_len = flight_detail.length();
+            largest_len = flight_len > largest_len ? flight_len : largest_len;
+            ++it;
+            print_data[i + 1].push_back(flight_detail);
+        }
+
+        if (is_less) {
+            for (size_t i = num_flights; i < print_data.size() - 1; i++) {
+                print_data[i + 1].push_back("");
+            }
+        }
+    }
+
+    for (size_t prt = 0; prt < print_data.size(); prt++) {
+        if (prt == 0) {
+            vector<string>& airports = print_data[0];
+            for (size_t idx = 0; idx < airports.size() - 1; idx++) {
+                cout << airports[idx] << setw(9 + largest_len);
+            }
+            cout << airports[airports.size() - 1] << endl;
+        } else {
+            vector<string>& flight_det = print_data[prt];
+            cout << setfill(' ') << setw(5 + largest_len);
+            for (size_t idx = 0; idx < flight_det.size(); idx++) {
+                cout << flight_det[idx] << setfill(' ') << setw(9 + largest_len);
+            }
+            cout << endl;
+        }
+    }
+}
